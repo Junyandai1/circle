947e22327ead6a5018f6f8697a971853e016fc3b
Junyan(Eric)Dai <148453921+Junyandai1@users.noreply.github.com>
2024-01-26 00:07:27 -0500

    Update DetectCircle.py

--- DetectCircle.py
+++ DetectCircle.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 # read image
-image = cv2.imread(r'C:\Users\cnbnu\Desktop\can.jpg')
+image = cv2.imread('/Users/daijunyan/Desktop/WechatIMG366.jpg')
 
 # convert to gray image
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -16,13 +16,13 @@ circles = cv2.HoughCircles(gray,
                            param2=100, 
                            minRadius=90, 
                            maxRadius=250)
-print(circles.shape)
+
 # overlay circles on the original image
 if circles is not None:
     circles = np.round(circles[0, :]).astype("int")
     for (x, y, r) in circles:
         cv2.circle(image, (x, y), r, (0, 255, 0), 4)
-        cv2.rectangle(image, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+        cv2.circle(image, (x, y), 1, (0, 255, 0), -1)
 
 # save image
-cv2.imwrite(r'C:\Users\cnbnu\Desktop\can_circle.jpg', image)
+cv2.imwrite('/Users/daijunyan/Desktop/WechatIMG366.jpg', image)
